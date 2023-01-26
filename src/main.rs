8593caeefe3e4c6d736908b6252e1568cc2a718f
Susa Milán <smlan@cock.li>
2023-01-26 14:39:27 +0100

    Added the base of the crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod models;
+
+use actix_web::{middleware, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting Git HTTP server at http://0.0.0.0:4545");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(api::git::info_refs)
+            .service(api::git::service_rpc)
+    })
+    .bind(("0.0.0.0", 4545))?
+    .run()
+    .await
 }
